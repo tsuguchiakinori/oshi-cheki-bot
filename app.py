2d90e7e20649482b8da02119ec599e95be8922ed
tsuguchiakinori <tsuguchi.work@gmail.com>
2026-05-02 22:42:30 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+
+app = Flask(__name__)
+
+# ←ここにさっきのやつ貼る
+CHANNEL_ACCESS_TOKEN = "EecvJy3p8FakrBHR+YSq/DD3MgCyVqVOQTBPKaa498vy8K/zvXWC6TCuh5zWL8Z0lhn4FE90x7agy/oP6FLv0cxvQLW4oK6fEqELMU0pdbsOgRaBsKouFT6w+QO0UdwwySuRyTpzjxKjyDnf/7kXRwdB04t89/1O/w1cDnyilFU="
+CHANNEL_SECRET = "38b1bd16322ce948e132554848286444"
+
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    user_message = event.message.text
+
+    # 👇ここ自由にいじれる
+    if "チェキ" in user_message:
+        reply = "推しチェキいいね！📸✨"
+    else:
+        reply = f"{user_message} って言ったね！"
+
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text=reply)
+    )
+
+if __name__ == "__main__":
+    app.run()
